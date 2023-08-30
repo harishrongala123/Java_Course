3b913d08b76a1a21831bca97bca56a2e18d561d3
haris <harishrongala@gmail.com>
2023-08-30 17:23:54 +0530

    Java course

--- src/Z24.java
+++ src/Z24.java
@@ -2,7 +2,7 @@ public class Z24 {
     public static void main(String[] args) {
         int [] elements = {10,20,30,500,700};
         for (int i: elements) {
-            System.out.println(i);
+            System.out.println(i);//
         }
     }
 }
